889adc41044672084f787ab0e7f1e9ef950bd63d
Jigy <janweljigycastillo20@gmail.com>
2022-03-05 13:08:07 +0800

    done activity

--- StudentList.ts
+++ StudentList.ts
@@ -60,10 +60,27 @@ export class StudentList {
 
     updateCollege(id: string, newCollegeName: string) {
         // find student using id
+        let student = this.students.find(element => element.studentId === id);
+
+        if (student) {
+            student.college = newCollegeName;
+            return student;
+        }
+
+        return "Student id doesnt exists";
+        
     }
 
     updateAddress(id: string, newAddress: string) {
         // find student using id
+        let student = this.students.find(element => element.studentId === id);
+
+        if (student) {
+            student.address = newAddress;
+            return student;
+        }
+
+        return "Student id doesnt exists";
     }
 
     updateCourse(id: string, newCourse: string) {

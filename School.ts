b44ebecec70ce6890235d5c0fb89e251244ddb81
Jigy <janweljigycastillo20@gmail.com>
2022-03-05 15:48:39 +0800

    done activity

--- School.ts
+++ School.ts
@@ -36,15 +36,17 @@ const studentList = new StudentList([
     student2,
     student3
 ]);
-
+console.log(`Before Removed, Student ${studentList.numberOfStudents()}`);
+//console.log(studentList.numberOfStudents());
 console.log(studentList.getStudents());
 // console.log("*************");
 // console.log(studentList.updateStudentName("2015-105869", "Michael Jordan"));
 // console.log("*************");
 // console.log(studentList.checkStudentId("2015-105870"));
-console.log(studentList.updateCourse("2015-105869", "BSIT"));
-console.log(studentList.updateCollege("2015-105869", "Computer" ));
+console.log(studentList.updateCourse("2015-105869", "BSBA"));
+console.log(studentList.updateCollege("2015-105869", "COB" ));
 console.log(studentList.updateAddress("2015-105869", "Cainta"));
-
 console.log(studentList.getStudents());
+studentList.removeStudent("2015-105868")
+console.log(`After Removed, Student ${studentList.numberOfStudents()}`);
 
--- StudentList.ts
+++ StudentList.ts
@@ -2,109 +2,82 @@ import { Student } from "./Student";
 
 export class StudentList {
     students: Array<Student>;
-
     constructor(students: Array<Student>) {
         this.students = students
     }
-
     getStudents() {
         return this.students;
     }
-
     updateStudentName(id: string, name: string) {
         if (this.checkStudentId(id)) {
             for (let index = 0; index < this.students.length; index++) {
                 const element = this.students[index];
-
                 if (element.studentId === id) {
                     element.name = name;
                     return element;
                 }
             }
         }
-
         return "Student id doesnt exists";
     }
-
     checkStudentId(id: string) {
-        // let valid = false;
-
-        // for (let index = 0; index < this.students.length; index++) {
-        //     const element = this.students[index];
-
-        //     if (element.studentId === id) {
-        //         valid = true;
-        //     }
-        // }
         let checkStudent = this.students.find(element => element.studentId === id);
-
         if (checkStudent) return true;
-
         return false;
     }
-
     updateAge(id: string, age: number) {
         let student = this.students.find(element => element.studentId === id);
-
         if (student) {
             student.age = age;
             return student;
         }
-
         return "Student id doesnt exists";
     }
-
+    numberOfStudents(){
+        const count = this.students.length
+        return `Count: ${count}`
+}
     removeStudent(id: string) {
-        // this.students.splice(index, 1)
+        if (this.checkStudentId(id)) {
+            for (let index = 0; index < this.students.length; index++) {
+                const element = this.students[index];
+                if (element.studentId === id) {
+                    this.students.splice(index, index)
+                    return element;
+            }
+        }
     }
-
+    }
     updateCollege(id: string, newCollegeName: string) {
-        // find student using id
         let student = this.students.find(element => element.studentId === id);
-
         if (student) {
             student.college = newCollegeName;
             return student;
         }
-
         return "Student id doesnt exists";
-        
     }
-
     updateAddress(id: string, newAddress: string) {
-        // find student using id
         let student = this.students.find(element => element.studentId === id);
-
         if (student) {
             student.address = newAddress;
             return student;
         }
-
         return "Student id doesnt exists";
     }
-
     updateCourse(id: string, newCourse: string) {
-        // find student using id
         let student = this.students.find(element => element.studentId === id);
-
         if (student) {
             student.course = newCourse;
             return student;
         }
-
         return "Student id doesnt exists";
     }
-
     updateGender(id: string, newGender: string) {
-        // find student using id
         let student = this.students.find(element => element.studentId === id);
-
         if (student) {
             student.gender = newGender;
             return student;
         }
-
         return "Student id doesnt exists";
-        
     }
 }
